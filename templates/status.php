c96948443bc66a6caffe49742199f884496167ed
Kedar <kedar54@gmail.com>
2013-11-20 19:54:23 -0600

    Status Changes
    Added "online" styling
    Added if check to see if the number of players online is greater than
    one. If this is true, then "s" is echoed

--- templates/status.php
+++ templates/status.php
@@ -1,5 +1,5 @@
-Server is Online<br/>
-<?php echo $onlineppl; ?> Players Online<br/>
+Server Status: <span class="label label-success">Online</span><br/>
+<?php echo $onlineppl . " Player"; if ($onlineppl > 1) {echo "s";}?> Online<br/>
 <?php echo $exp . " EXP";?><br/>
 <?php echo $meso . " MESO";?><br/>
 <?php echo $drop . " DROP";?><br/>
